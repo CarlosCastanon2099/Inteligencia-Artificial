ed7b6890cf30147731c7ea99c5b0825a2a11050e
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2024-03-10 20:34:29 -0600

    Proyecto 01 - Ahorcado-Programa Completado
    Co-authored-by: DafneBonilla <DafneBonilla@users.noreply.github.com>
    Co-authored-by: pablocro <pablocro@users.noreply.github.com>
    Co-authored-by: HoracioPai <HoracioPai@users.noreply.github.com>

--- Proyectos/Proyecto-01/Ahorcado.py
+++ Proyectos/Proyecto-01/Ahorcado.py
@@ -8,18 +8,18 @@ MAXINTENTOS = 6
 def iniciar_juego(palabra, algoritmo):
     
     letras_usadas = []
-    print("La palabra a adivinar es: ", palabra)
-    print("El algoritmo seleccionado es: ", algoritmo_a_nombre(algoritmo))
+    print("La palabra a adivinar es: \t", palabra)
+    print("El algoritmo seleccionado es: ", algoritmo_a_nombre(algoritmo), "\n")
 
     errores = 0
 
-    print("Que empieze el juego manco")
+    print("Que empieze el juego! :D\n")
 
     palabra_clave = '_' * len(palabra)
     letra = ""
 
     while True:
-        print("Correcta: " + " ".join(palabra_clave))
+        print("Palabra\t: " + " ".join(palabra_clave))
         if algoritmo == '1':
             letra = no_informada(palabra_clave, letras_usadas)
         elif algoritmo == '2':
@@ -32,19 +32,20 @@ def iniciar_juego(palabra, algoritmo):
 
         if letra not in palabra:
             errores += 1
-            print("No le atino el bot este, tiene {} intentos aun".format(MAXINTENTOS - errores))
+            print("\nLetra erronea, por lo tanto quedan {} intentos\n".format(MAXINTENTOS - errores))
 
             if errores == MAXINTENTOS:
-                print("Fracaso por falta de intentos, La respuesta es: {}" .format(palabra))
+                print("\nFracaso por falta de intentos, La respuesta es: {}\n" .format(palabra))
                 break
             
         else:
+            print("\nLa letra \'" + letra + "\' es correcta!!\n")
             for i in range(len(palabra)):
                 if palabra[i] == letra:
                     palabra_clave = palabra_clave[:i] + letra + palabra_clave[i+1:] 
 
             if "_" not in palabra_clave:
-                    print("Sorependentemente lo ha logrado WAOS")
+                    print("Sorependentemente lo ha logrado!!! :D")
                     break
 
 """
@@ -72,7 +73,7 @@ Funcion para mostrar el menu de inicio
 """  
 def mostrar_menu():
     print("\n¡Bienvenido al juego de ahorcado!\n")
-    print("Ingrese una palabra menor a 6 letras y sin acentos para que sea adivinada:")
+    print("Ingrese una palabra menor a 6 letras y sin acentos para que sea adivinada:\n")
     while True:
         palabra = input()
         if len(palabra) > 5:
@@ -105,14 +106,14 @@ Funcion para mostrar el menu de algoritmos
 <return> numero de algoritmo
 """  
 def mostrar_algoritmos():            
-    print("\n¡Ahora elige el algoritmo que deseas usar para adivinar la palabra!\n")
+    print("\n¡Ahora elige la opción del algoritmo que deseas usar para adivinar la palabra!\n")
     print("1. Algoritmo de busqueda no informada")
     print("2. Algoritmo de busqueda informada")
     print("3. Algoritmo genetico")
     while True:
         algoritmo = input()
         if algoritmo in ['1','2','3']:
-            print("\n¡Elegiste el %s!" % algoritmo_a_nombre(algoritmo))
+            print("\n¡Elegiste el %s!\n" % algoritmo_a_nombre(algoritmo))
         else:
             try:
                 print("\nOops! Opcion no valida. Por favor, selecciona una opcion valida del menu:")
@@ -121,6 +122,30 @@ def mostrar_algoritmos():
                 print("Ocurrio un error:", e)
                 continue
         return algoritmo
+    
+"""
+Funcion para pedirle al usuario si quieres volver a jugar
+"""  
+def salir_juego():            
+    print("\n¿Quieres seguir jugando?\nElija el numero de una de opciones:\n")
+    print("1. Si")
+    print("2. No")
+    while True:
+        opcion = input()
+        if opcion == '1':
+            break
+        if opcion == '2':
+            print("\n\nGracias por jugar al ahorcado!!\nVuelva pronto!!")
+            break
+        else:
+            try:
+                print("\nOops! Opcion no valida. Por favor, selecciona una opcion valida del menu:")
+                continue
+            except Exception as e:
+                print("Ocurrio un error:", e)
+                continue
+        break
+    return opcion
 
 """
 Funcion para obtener el nombre del algoritmo a partir de su numero
@@ -140,9 +165,12 @@ def algoritmo_a_nombre(numero_algoritmo):
 Funcion inicial para ejecutar el algoritmo del ahorcado
 """
 if __name__ == "__main__":
-    palabra = mostrar_menu()
-    algoritmo = mostrar_algoritmos()
-    iniciar_juego(palabra, algoritmo)
+    salir = 0
+    while(salir != '2'):
+        palabra = mostrar_menu()
+        algoritmo = mostrar_algoritmos()
+        iniciar_juego(palabra, algoritmo)
+        salir = salir_juego()
 
 """   
 Diccionario de palabras en español tomado de https://github.com/JorgeDuenasLerin/diccionario-espanol-txt
