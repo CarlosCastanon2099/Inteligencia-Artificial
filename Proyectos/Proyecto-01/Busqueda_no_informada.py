171028c7779290b10a704bd70043bd02a6c1fd55
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2024-03-10 20:26:17 -0600

    Proyecto 01 - Busqueda No Informada Definitivo
    Co-authored-by: DafneBonilla <DafneBonilla@users.noreply.github.com>
    Co-authored-by: pablocro <pablocro@users.noreply.github.com>
    Co-authored-by: HoracioPai <HoracioPai@users.noreply.github.com>

--- Proyectos/Proyecto-01/Busqueda_no_informada.py
+++ Proyectos/Proyecto-01/Busqueda_no_informada.py
@@ -1,17 +1,19 @@
-# ya jalo :D
+import random
 
 alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 
 def no_informada(palabra, usadas):
-    for letra in alfabeto:
-        if letra not in usadas:
+    while True:
+        letra = random.choice(alfabeto)
+        if letra not in usadas and letra not in palabra:
             return letra
-        
-    return None
+    return '?'
 
 
-
+'''
 if __name__ == "__main__":
-    palabra_jugada = "abc"
-    no_informada(palabra_jugada, ['a','b','c'])
+    palabra_jugada = "dona"
+    print(palabra_jugada)
+    print(no_informada(palabra_jugada, ['e','b','c']))
+'''
